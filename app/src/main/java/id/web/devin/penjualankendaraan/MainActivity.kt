277ca7f072ebd2c88ac38d4c6ec8efb718585101
devinriyanti <devinsp4m@gmail.com>
2023-11-17 20:41:28 +0700

    2nd

--- app/src/main/java/id/web/devin/penjualankendaraan/MainActivity.kt
+++ app/src/main/java/id/web/devin/penjualankendaraan/MainActivity.kt
@@ -2,10 +2,29 @@ package id.web.devin.penjualankendaraan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import id.web.devin.penjualankendaraan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b:ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavHost) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        NavigationUI.setupActionBarWithNavController(this, navController, b.drawerLayout)
+        NavigationUI.setupWithNavController(b.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, b.drawerLayout) || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/id/web/devin/penjualankendaraan/MainFragment.kt
@@ -0,0 +1,20 @@
+package id.web.devin.penjualankendaraan
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.web.devin.penjualankendaraan.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var b:FragmentMainBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        b = FragmentMainBinding.inflate(layoutInflater)
+        return b.root
+    }
+}
