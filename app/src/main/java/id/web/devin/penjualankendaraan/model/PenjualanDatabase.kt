981dd8e1e8593ce391dd874f585e7c4d5148aee9
devinriyanti <devinsp4m@gmail.com>
2023-11-19 22:49:39 +0700

    4th

--- app/src/main/java/id/web/devin/penjualankendaraan/model/PenjualanDatabase.kt
+++ app/src/main/java/id/web/devin/penjualankendaraan/model/PenjualanDatabase.kt
@@ -1,5 +1,28 @@
 package id.web.devin.penjualankendaraan.model
 
-class PenjualanDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
+@Database(entities = arrayOf(Kendaraan::class), version = 1)
+abstract class PenjualanDatabase:RoomDatabase() {
+    abstract fun penjualanDAO(): PenjualanDAO
+    companion object{
+        @Volatile private var instance:PenjualanDatabase ?= null
+        private val LOCK = Any()
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            PenjualanDatabase::class.java,
+            "penjualandb"
+        ).build()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: buildDatabase(context).also {
+                instance = it
+            }
+            buildDatabase(context)
+        }
+    }
 }
